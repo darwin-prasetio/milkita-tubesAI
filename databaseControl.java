6bd9addffb5bab93d577a63ab08dab882473a205
Darwin <prasetiodarwin@gmail.com>
2014-11-21 20:25:31 +0700

    setup ke database udah jadi

---
+++ databaseControl.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tubes;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import weka.core.Instances;
+import weka.core.converters.DatabaseLoader;
+
+/**
+ *
+ * @author user
+ */
+public class databaseControl {
+    private static databaseControl loader=null;
+    private String URL;
+    private final String USER,PASS;
+    private final DatabaseLoader connect;
+    
+    public databaseControl() throws Exception{
+        this.connect = new DatabaseLoader();
+        this.URL="jdbc:mysql://localhost/";
+        this.USER="root";
+        this.PASS="";
+    }
+    
+    public static databaseControl getInstance() throws Exception{
+        if(loader==null){
+            loader = new databaseControl();
+        }
+        return loader;
+    }
+    
+    public void setDatabase(String databaseName){
+        this.URL="jdbc:mysql://localhost/"+databaseName;
+    }
+    
+    public void setConnection() throws Exception{
+        if(loader!=null){
+            connect.setSource(this.URL,this.USER,this.PASS);
+        }
+    }
+    
+    public void selectQuery(String tableName, ArrayList<String> columns, String condition){
+        String query = new String("SELECT ");
+        for(int i=0;i<columns.size();i++){
+            query = query+columns.get(i);
+            if(i!=columns.size()-1){
+                query += ",";
+            }
+        }
+        query+=" FROM " + tableName;
+        if(condition!=null){
+            query+= " WHERE " + condition;
+        }
+        connect.setQuery(query);
+    }
+    public Instances getData() throws IOException{
+        return connect.getDataSet();
+    }
+    public void getDB(){
+        System.out.println(this.URL);
+    }
+}
