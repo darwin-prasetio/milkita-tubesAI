a0a5f9bfdb5f3fc39538e6d03183059560f1e605
Darwin <prasetiodarwin@gmail.com>
2014-11-22 23:50:00 +0700

    contoh databaseControl

--- milkita/src/milkita/Milkita.java
+++ milkita/src/milkita/Milkita.java
@@ -6,17 +6,30 @@
 
 package milkita;
 
+import java.util.ArrayList;
+import weka.core.Instances;
+
 /**
  *
  * @author user
  */
 public class Milkita {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static databaseControl db;
+    private static Instances testInstance;
+    public static void main(String[] args) throws Exception {
+        db.getInstance(); // konstruktor databaseControl
+        db.setDatabase("news_aggregator");
+        db.setConnection(); //melakukan koneksi terhadap database
+        db.selectQuery("ARTIKEL JOIN ARTIKEL_KATEGORI_VERIFIED JOIN KATEGORI", new ArrayList<String>() {{
+            add("ID_ARTIKEL");
+            add("ARTIKEL");
+            add("JUDUL");
+            add("ID_KELAS");
+            add("KATEGORI");}}, null);
+        testInstance=new Instances(db.getData());
+        for(int i=0;i<testInstance.numInstances();i++){ // print instances
+            System.out.println(testInstance.instance(i));
+        }
     }
     
 }
