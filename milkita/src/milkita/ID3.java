f95c1540f6c3ebe96da30aad7dbde0e0571c194c
Darwin <prasetiodarwin@gmail.com>
2014-11-22 23:30:25 +0700

    tambah komentar

--- milkita/src/milkita/ID3.java
+++ milkita/src/milkita/ID3.java
@@ -13,10 +13,7 @@ import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import java.util.Random;
 
-/**
- *
- * @author user
- */
+/* Membangun model pembelajaran dengan ID3 / pohon keputusan */
 public class ID3 {
     private static J48 tree;
     private Evaluation evaluator,evaluator_2;
@@ -27,6 +24,7 @@ public class ID3 {
     }
     
     public void classifyInstance(){
+        // mengklasifisikan data yang belum diketahui kelasnya
         labeled = new Instances(instance);
         labeled.setClassIndex(labeled.numAttributes()-1);
         double iLabel=0;
@@ -45,6 +43,8 @@ public class ID3 {
             evaluator = new Evaluation(instance);
             tree.buildClassifier(instance);
             crossValidate();
+            
+            //evaluasi model hipotesis
             evaluator.evaluateModel(tree,instance);
         } catch (Exception ex) {
             Logger.getLogger(ID3.class.getName()).log(Level.SEVERE, null, ex);
@@ -52,6 +52,7 @@ public class ID3 {
     }
 
     private void crossValidate() throws Exception {
+        //membangun model hipotesis dari training data yang diberikan
         evaluator_2 = new Evaluation(instance);
         evaluator_2.crossValidateModel(tree, labeled, 10,new Random(1));
     }
