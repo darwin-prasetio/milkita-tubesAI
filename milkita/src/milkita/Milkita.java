fbff08c10ad0ee6bad6ccb07428bb6eeff0b5226
opelhoward <opelhoward@yahoo.com>
2014-11-27 18:08:52 +0700

    add StringToVector

--- milkita/src/milkita/Milkita.java
+++ milkita/src/milkita/Milkita.java
@@ -3,38 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package milkita;
 
 import java.util.ArrayList;
+import weka.core.Attribute;
+import weka.core.Instance;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NominalToString;
+import weka.filters.unsupervised.attribute.StringToWordVector;
 
 /**
  *
  * @author user
  */
 public class Milkita {
-    private static databaseControl db;
-    private static Instances testInstance;
-    private static String s;
+
     public static void main(String[] args) throws Exception {
-        db = new databaseControl(); // konstruktor databaseControl
-        db.setDatabase("news_aggregator");
+        DatabaseController db = new DatabaseController();         db.setDatabase("news_aggregator");
         db.setConnection(); //melakukan koneksi terhadap database
-        db.selectQuery("ARTIKEL NATURAL JOIN ARTIKEL_KATEGORI_VERIFIED NATURAL JOIN KATEGORI", new ArrayList<String>() {{
-            add("ARTIKEL.ID_ARTIKEL");
-            add("JUDUL");
-            add("KATEGORI.LABEL");
-            add("ARTIKEL.FULL_TEXT");}}, null);
-        testInstance = new Instances(db.getData());
-        for(int i=0;i<testInstance.numInstances();i++){ // filter
-            s=testInstance.instance(i).stringValue(3);
-            System.out.println(s);
-            s = Filtering.Filter(s);
-            System.out.println(s);
-            System.out.println(testInstance.instance(i).stringValue(2));
-            System.out.println();
+        db.selectQuery("ARTIKEL NATURAL JOIN ARTIKEL_KATEGORI_VERIFIED NATURAL JOIN KATEGORI", new ArrayList<String>() {
+            {
+                add("ARTIKEL.ID_ARTIKEL");
+                add("JUDUL");
+                add("KATEGORI.LABEL");
+                add("ARTIKEL.FULL_TEXT");
+            }
+        }, null);
+        Instances instances = new Instances(db.getData());
+        
+        NominalToString filterNominalToString = new NominalToString();
+        filterNominalToString.setInputFormat(instances);
+        
+        instances = Filter.useFilter(instances, filterNominalToString);
+        for (int it = 0; it < instances.numInstances(); ++it) {
+            Instance curr = instances.instance(it);
+            curr.setValue(3, Utility.Filtering(curr.stringValue(3)));
         }
+
+        StringToWordVector filterStringToVector = new StringToWordVector();
+        String[] optionsStringToVector = "-R 4".split(" ");
+        filterStringToVector.setOptions(optionsStringToVector);
+        filterStringToVector.setInputFormat(instances);
+        instances = Filter.useFilter(instances, filterStringToVector);
+        
     }
-    
 }
---
+++ milkita/src/milkita/Utility.java
@@ -0,0 +1,48 @@
+package milkita;
+
+public class Utility {
+
+    public static String Filtering(String s) {
+        String hasil;
+        hasil = s.toLowerCase();
+		//Syarat filternya. Ini baru ngambil , . ? " / (pake regex)
+        //todo, ngambil kata-kata gak pentingnya
+        hasil = hasil.replaceAll("[,|\\.|\\?|\"|/]", " ");
+
+        //ngilangin enter
+        hasil = hasil.replaceAll("\n", " ");
+
+        //ngambil angka? kayaknya gak penting deh
+        hasil = hasil.replaceAll("\\d", " ");
+
+        //hilangin hari
+        hasil = hasil.replaceAll(" senin | selasa | rabu | kamis | jumat | sabtu | minggu ", " ");
+
+        //kata subjek
+        hasil = hasil.replaceAll(" saya | anda | kamu | dia | mereka | kita | kami | aku ", " ");
+
+        //ngambil kata hubung, ini kata hubung yang ada di jelasin di wikipedia
+        hasil = hasil.replaceAll(" tepatnya | di | dilaksanakan | menggunakan | menjadi | tetap | akhirnya |\\s?merupakan\\s? | seharusnya | ternyata | hanya | lainnya | tersebut | yang | agar | supaya | biar | jika | kalau | jikalau | asal | asalkan | bila | manakala | sejak | semenjak | sedari | sewaktu | tatkala | ketika | sementara | begitu | seraya | selagi | selama | serta | sambil | demi | setelah | sesudah | sebelum | sehabis | selesai | seusai | hingga | sampai | andaikan | seandainya | umpamanya | sekiranya | biar | biarpun | walau | walaupun | sekali | sekalipun | sungguhpun | kendati | kendatipun | seakan-akan | seolah-olah | seakan | seolah | sebagaimana | seperti | sebagai | laksana | sedangkan | ibarat | daripada | alih-alih | sebab | karena | oleh | sehingga | sampai | maka | dengan | tanpa | bahwa ", " ");
+
+        //ngambil bulan, kayaknya gak penting juga
+        hasil = hasil.replaceAll(" besok | kemarin | hari | januari | februari | pebruari | maret | april | mei | juni | juli | agustus | september | oktober | november | nopember | desember ", " ");
+
+        //ngambil kata-gak-penting, tapi gatau masuk kategori apa
+        hasil = hasil.replaceAll(" juga | sudah | adalah | kata | tapi | depan | belakang |\\s?akan\\s? | ini | itu | tak | tidak | dari | dan | pada | untuk ", " ");
+		//ngambil kata hubung (gak penting kan?)
+
+        //ngambil tanda hubung yang berdiri sendiri (laba-laba) sama (laba) kan beda
+        hasil = hasil.replaceAll(" - |\\s?--\\s?", " ");
+
+        //kata yang menyatakan bilangan dan uang
+        hasil = hasil.replaceAll("satu | dua | tiga | empat | lima | enam | tujuh | delapan | sembilan | sepuluh | puluhan |"
+                + " ratusan | ribuan | jutaan | milyar | milyaran | miliar | miliaran | ratus | ribu | rupiah | rp ", " ");
+
+        hasil = hasil.replaceAll("\\s?republika co id |\\s?metrotvnews com | \\(antara news\\)- |perusahaan | pt | aneka |tempo co ", " ");
+
+        //ngehilangnin multiple white space jadi satu aja
+        hasil = hasil.replaceAll("\\s+", " ");
+        return hasil;
+    }
+
+}
